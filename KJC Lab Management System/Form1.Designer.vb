7c3713a1bd1c27118e21ec55c15f999cb7ab18be
Joker1198 <gerogepshaiju@gmail.com>
2024-01-06 01:53:10 +0530

    Add project files.

---
+++ KJC Lab Management System/Form1.Designer.vb
@@ -0,0 +1,73 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        txtUsername = New TextBox()
+        txtPassword = New TextBox()
+        btnLogin = New Button()
+        SuspendLayout()
+        ' 
+        ' txtUsername
+        ' 
+        txtUsername.Location = New Point(239, 164)
+        txtUsername.Name = "txtUsername"
+        txtUsername.PlaceholderText = "Username"
+        txtUsername.Size = New Size(285, 23)
+        txtUsername.TabIndex = 0
+        ' 
+        ' txtPassword
+        ' 
+        txtPassword.Location = New Point(239, 224)
+        txtPassword.Name = "txtPassword"
+        txtPassword.PlaceholderText = "Password"
+        txtPassword.Size = New Size(285, 23)
+        txtPassword.TabIndex = 1
+        ' 
+        ' btnLogin
+        ' 
+        btnLogin.Location = New Point(318, 286)
+        btnLogin.Name = "btnLogin"
+        btnLogin.Size = New Size(113, 23)
+        btnLogin.TabIndex = 2
+        btnLogin.Text = "Sign-in"
+        btnLogin.UseVisualStyleBackColor = True
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(7F, 15F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(800, 450)
+        Controls.Add(btnLogin)
+        Controls.Add(txtPassword)
+        Controls.Add(txtUsername)
+        Name = "Form1"
+        Text = "Form1"
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents txtUsername As TextBox
+    Friend WithEvents txtPassword As TextBox
+    Friend WithEvents btnLogin As Button
+
+End Class
---
+++ KJC Lab Management System/Form1.vb
@@ -0,0 +1,34 @@
+﻿Imports MySql.Data.MySqlClient
+
+Public Class Form1
+    Dim connectionString As String = "Server=127.0.0.1;Database=kjclab;User Id=root;Password=;"
+
+    Private Sub btnLogin_Click(sender As Object, e As EventArgs) Handles btnLogin.Click
+        Dim username As String = txtUsername.Text
+        Dim password As String = txtPassword.Text
+
+        Try
+            If AuthenticateUser(username, password) Then
+                MessageBox.Show("Login Successful!", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information)
+                MessageBox.Show("Hello")
+            Else
+                MessageBox.Show("Invalid username or password", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
+            End If
+        Catch ex As Exception
+            MessageBox.Show("Error: " & ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
+        End Try
+    End Sub
+
+    Private Function AuthenticateUser(username As String, password As String) As Boolean
+        Using connection As New MySqlConnection(connectionString)
+            connection.Open()
+            Dim query As String = "SELECT COUNT(*) FROM login WHERE Username = @Username AND Password = @Password"
+            Using cmd As New MySqlCommand(query, connection)
+                cmd.Parameters.AddWithValue("@Username", username)
+                cmd.Parameters.AddWithValue("@Password", password)
+                Dim count As Integer = Convert.ToInt32(cmd.ExecuteScalar())
+                Return count > 0
+            End Using
+        End Using
+    End Function
+End Class

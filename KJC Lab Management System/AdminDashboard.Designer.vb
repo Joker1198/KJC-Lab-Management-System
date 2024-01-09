bcd1be9ffe81608d2d23ea499740f3f05c39b1cc
Joker1198 <gerogepshaiju@gmail.com>
2024-01-09 14:02:15 +0530

    .

--- KJC Lab Management System/AdminDashboard.Designer.vb
+++ KJC Lab Management System/AdminDashboard.Designer.vb
@@ -32,6 +32,12 @@ Partial Class AdminDashboard
         cmbNewRole = New ComboBox()
         Label3 = New Label()
         btnConfirmAddUser = New Button()
+        DataGridView1 = New DataGridView()
+        VmUser = New Button()
+        btnDeleteUser = New Button()
+        btnChangePassword = New Button()
+        btnChangeRole = New Button()
+        CType(DataGridView1, ComponentModel.ISupportInitialize).BeginInit()
         SuspendLayout()
         ' 
         ' btnLogout
@@ -55,14 +61,14 @@ Partial Class AdminDashboard
         ' 
         btnAddUsers.Location = New Point(35, 82)
         btnAddUsers.Name = "btnAddUsers"
-        btnAddUsers.Size = New Size(68, 59)
+        btnAddUsers.Size = New Size(90, 59)
         btnAddUsers.TabIndex = 2
         btnAddUsers.Text = "Add Users"
         btnAddUsers.UseVisualStyleBackColor = True
         ' 
         ' txtNewUsername
         ' 
-        txtNewUsername.Location = New Point(588, 207)
+        txtNewUsername.Location = New Point(259, 82)
         txtNewUsername.Name = "txtNewUsername"
         txtNewUsername.PlaceholderText = "College Mail"
         txtNewUsername.Size = New Size(200, 23)
@@ -70,7 +76,7 @@ Partial Class AdminDashboard
         ' 
         ' txtNewPassword
         ' 
-        txtNewPassword.Location = New Point(588, 259)
+        txtNewPassword.Location = New Point(259, 134)
         txtNewPassword.Name = "txtNewPassword"
         txtNewPassword.PlaceholderText = "Assign Password"
         txtNewPassword.Size = New Size(200, 23)
@@ -79,7 +85,7 @@ Partial Class AdminDashboard
         ' Label1
         ' 
         Label1.AutoSize = True
-        Label1.Location = New Point(516, 210)
+        Label1.Location = New Point(187, 85)
         Label1.Name = "Label1"
         Label1.Size = New Size(66, 15)
         Label1.TabIndex = 5
@@ -88,7 +94,7 @@ Partial Class AdminDashboard
         ' Label2
         ' 
         Label2.AutoSize = True
-        Label2.Location = New Point(519, 267)
+        Label2.Location = New Point(190, 142)
         Label2.Name = "Label2"
         Label2.Size = New Size(63, 15)
         Label2.TabIndex = 6
@@ -97,7 +103,7 @@ Partial Class AdminDashboard
         ' cmbNewRole
         ' 
         cmbNewRole.FormattingEnabled = True
-        cmbNewRole.Location = New Point(588, 309)
+        cmbNewRole.Location = New Point(259, 184)
         cmbNewRole.Name = "cmbNewRole"
         cmbNewRole.Size = New Size(200, 23)
         cmbNewRole.TabIndex = 7
@@ -105,7 +111,7 @@ Partial Class AdminDashboard
         ' Label3
         ' 
         Label3.AutoSize = True
-        Label3.Location = New Point(541, 312)
+        Label3.Location = New Point(212, 187)
         Label3.Name = "Label3"
         Label3.Size = New Size(41, 15)
         Label3.TabIndex = 8
@@ -113,18 +119,67 @@ Partial Class AdminDashboard
         ' 
         ' btnConfirmAddUser
         ' 
-        btnConfirmAddUser.Location = New Point(610, 365)
+        btnConfirmAddUser.Location = New Point(281, 240)
         btnConfirmAddUser.Name = "btnConfirmAddUser"
         btnConfirmAddUser.Size = New Size(153, 23)
         btnConfirmAddUser.TabIndex = 9
         btnConfirmAddUser.Text = "Confirm And Add User"
         btnConfirmAddUser.UseVisualStyleBackColor = True
         ' 
+        ' DataGridView1
+        ' 
+        DataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        DataGridView1.Location = New Point(440, 82)
+        DataGridView1.Name = "DataGridView1"
+        DataGridView1.Size = New Size(348, 171)
+        DataGridView1.TabIndex = 10
+        ' 
+        ' VmUser
+        ' 
+        VmUser.Location = New Point(35, 175)
+        VmUser.Name = "VmUser"
+        VmUser.Size = New Size(90, 59)
+        VmUser.TabIndex = 11
+        VmUser.Text = "View/Manage Users"
+        VmUser.UseVisualStyleBackColor = True
+        ' 
+        ' btnDeleteUser
+        ' 
+        btnDeleteUser.Location = New Point(700, 204)
+        btnDeleteUser.Name = "btnDeleteUser"
+        btnDeleteUser.Size = New Size(90, 49)
+        btnDeleteUser.TabIndex = 12
+        btnDeleteUser.Text = "Delete User"
+        btnDeleteUser.UseVisualStyleBackColor = True
+        ' 
+        ' btnChangePassword
+        ' 
+        btnChangePassword.Location = New Point(700, 142)
+        btnChangePassword.Name = "btnChangePassword"
+        btnChangePassword.Size = New Size(90, 44)
+        btnChangePassword.TabIndex = 13
+        btnChangePassword.Text = "Change Password"
+        btnChangePassword.UseVisualStyleBackColor = True
+        ' 
+        ' btnChangeRole
+        ' 
+        btnChangeRole.Location = New Point(700, 82)
+        btnChangeRole.Name = "btnChangeRole"
+        btnChangeRole.Size = New Size(90, 44)
+        btnChangeRole.TabIndex = 14
+        btnChangeRole.Text = "Change Role"
+        btnChangeRole.UseVisualStyleBackColor = True
+        ' 
         ' AdminDashboard
         ' 
         AutoScaleDimensions = New SizeF(7F, 15F)
         AutoScaleMode = AutoScaleMode.Font
         ClientSize = New Size(800, 450)
+        Controls.Add(btnChangeRole)
+        Controls.Add(btnChangePassword)
+        Controls.Add(btnDeleteUser)
+        Controls.Add(VmUser)
+        Controls.Add(DataGridView1)
         Controls.Add(btnConfirmAddUser)
         Controls.Add(Label3)
         Controls.Add(cmbNewRole)
@@ -137,6 +192,7 @@ Partial Class AdminDashboard
         Controls.Add(btnLogout)
         Name = "AdminDashboard"
         Text = "AdminDashboard"
+        CType(DataGridView1, ComponentModel.ISupportInitialize).EndInit()
         ResumeLayout(False)
         PerformLayout()
     End Sub
@@ -151,4 +207,9 @@ Partial Class AdminDashboard
     Friend WithEvents cmbNewRole As ComboBox
     Friend WithEvents Label3 As Label
     Friend WithEvents btnConfirmAddUser As Button
+    Friend WithEvents DataGridView1 As DataGridView
+    Friend WithEvents VmUser As Button
+    Friend WithEvents btnDeleteUser As Button
+    Friend WithEvents btnChangePassword As Button
+    Friend WithEvents btnChangeRole As Button
 End Class
--- KJC Lab Management System/AdminDashboard.vb
+++ KJC Lab Management System/AdminDashboard.vb
@@ -14,6 +14,10 @@ Public Class AdminDashboard
         Label1.Visible = False
         Label2.Visible = False
         Label3.Visible = False
+        DataGridView1.Visible = False
+        btnChangePassword.Visible = False
+        btnChangeRole.Visible = False
+        btnDeleteUser.Visible = False
     End Sub
 
     Private Sub btnAddUsers_Click(sender As Object, e As EventArgs) Handles btnAddUsers.Click
@@ -25,6 +29,10 @@ Public Class AdminDashboard
         Label1.Visible = True
         Label2.Visible = True
         Label3.Visible = True
+        DataGridView1.Visible = False ' Hide DataGridView when adding new users
+        btnChangePassword.Visible = False
+        btnChangeRole.Visible = False
+        btnDeleteUser.Visible = False
     End Sub
 
     Private Sub btnConfirmAddUser_Click(sender As Object, e As EventArgs) Handles btnConfirmAddUser.Click
@@ -72,6 +80,11 @@ Public Class AdminDashboard
             txtNewUsername.Clear()
             txtNewPassword.Clear()
             cmbNewRole.SelectedIndex = -1 ' Reset ComboBox selection
+
+            ' Show DataGridView after adding new user
+            DataGridView1.Visible = True
+            ' Fetch and display updated user data
+            LoadUserData()
         Catch ex As Exception
             MessageBox.Show("Error adding user: " & ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
         End Try
@@ -82,4 +95,175 @@ Public Class AdminDashboard
         Me.Close()
         Form1.Close()
     End Sub
+
+    Private Sub VmUser_Click(sender As Object, e As EventArgs) Handles VmUser.Click
+        Try
+            ' Display user data in the DataGridView
+            txtNewUsername.Visible = False
+            txtNewPassword.Visible = False
+            cmbNewRole.Visible = False
+            btnConfirmAddUser.Visible = False
+            Label1.Visible = False
+            Label2.Visible = False
+            Label3.Visible = False
+            DataGridView1.Visible = True
+            btnChangePassword.Visible = True
+            btnChangeRole.Visible = True
+            btnDeleteUser.Visible = True
+            LoadUserData()
+        Catch ex As Exception
+            MessageBox.Show("Error fetching user data: " & ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
+        End Try
+    End Sub
+
+    Private Sub LoadUserData()
+        Try
+            ' Fetch user data from the database
+            Dim userData As DataTable = GetUserData()
+
+            ' Display the user data in the DataGridView
+            DataGridView1.DataSource = userData
+        Catch ex As Exception
+            MessageBox.Show("Error fetching user data: " & ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
+        End Try
+    End Sub
+
+    ' Method to delete selected user
+    Private Sub DeleteUser()
+        If DataGridView1.SelectedRows.Count > 0 Then
+            Try
+                Dim selectedUsername As String = DataGridView1.SelectedRows(0).Cells("Username").Value.ToString()
+
+                ' Delete user from the database
+                Using connection As New MySqlConnection(Form1.connectionString)
+                    connection.Open()
+                    Dim deleteQuery As String = "DELETE FROM login WHERE Username = @Username"
+                    Using cmd As New MySqlCommand(deleteQuery, connection)
+                        cmd.Parameters.AddWithValue("@Username", selectedUsername)
+                        cmd.ExecuteNonQuery()
+                    End Using
+                End Using
+
+                ' Refresh the user data in the DataGridView
+                LoadUserData()
+
+                MessageBox.Show($"User '{selectedUsername}' deleted successfully.", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information)
+            Catch ex As Exception
+                MessageBox.Show("Error deleting user: " & ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
+            End Try
+        Else
+            MessageBox.Show("Please select a user to delete.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
+        End If
+    End Sub
+
+    ' Method to change role of selected user
+    Private Sub ChangeUserRole()
+        If DataGridView1.SelectedRows.Count > 0 Then
+            Try
+                Dim selectedUsername As String = DataGridView1.SelectedRows(0).Cells("Username").Value.ToString()
+
+                ' Show a dialog to input the new role
+                Dim newRole As String = InputBox($"Enter new role for '{selectedUsername}':", "Change Role", "Student")
+
+                If Not String.IsNullOrEmpty(newRole) Then
+                    ' Update the user role in the database
+                    Using connection As New MySqlConnection(Form1.connectionString)
+                        connection.Open()
+                        Dim updateQuery As String = "UPDATE login SET Role = @Role WHERE Username = @Username"
+                        Using cmd As New MySqlCommand(updateQuery, connection)
+                            cmd.Parameters.AddWithValue("@Role", newRole)
+                            cmd.Parameters.AddWithValue("@Username", selectedUsername)
+                            cmd.ExecuteNonQuery()
+                        End Using
+                    End Using
+
+                    ' Refresh the user data in the DataGridView
+                    LoadUserData()
+
+                    MessageBox.Show($"Role for '{selectedUsername}' changed to '{newRole}'.", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information)
+                End If
+            Catch ex As Exception
+                MessageBox.Show("Error changing user role: " & ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
+            End Try
+        Else
+            MessageBox.Show("Please select a user to change the role.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
+        End If
+    End Sub
+
+    ' Method to change password of selected user
+    Private Sub ChangeUserPassword()
+        If DataGridView1.SelectedRows.Count > 0 Then
+            Try
+                Dim selectedUsername As String = DataGridView1.SelectedRows(0).Cells("Username").Value.ToString()
+
+                ' Show a dialog to input the new password
+                Dim newPassword As String = InputBox($"Enter new password for '{selectedUsername}':", "Change Password", "")
+
+                If Not String.IsNullOrEmpty(newPassword) Then
+                    ' Update the user password in the database
+                    Using connection As New MySqlConnection(Form1.connectionString)
+                        connection.Open()
+                        Dim updateQuery As String = "UPDATE login SET Password = @Password WHERE Username = @Username"
+                        Using cmd As New MySqlCommand(updateQuery, connection)
+                            cmd.Parameters.AddWithValue("@Password", newPassword)
+                            cmd.Parameters.AddWithValue("@Username", selectedUsername)
+                            cmd.ExecuteNonQuery()
+                        End Using
+                    End Using
+
+                    ' Refresh the user data in the DataGridView
+                    LoadUserData()
+
+                    MessageBox.Show($"Password for '{selectedUsername}' changed successfully.", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information)
+                End If
+            Catch ex As Exception
+                MessageBox.Show("Error changing user password: " & ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
+            End Try
+        Else
+            MessageBox.Show("Please select a user to change the password.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
+        End If
+    End Sub
+
+    ' Button click events for managing users
+    Private Sub btnDeleteUser_Click(sender As Object, e As EventArgs)
+        ' Call the method to delete the selected user
+        DeleteUser()
+    End Sub
+
+    Private Sub btnChangeRole_Click(sender As Object, e As EventArgs)
+        ' Call the method to change the role of the selected user
+        ChangeUserRole()
+    End Sub
+
+    Private Sub btnChangePassword_Click(sender As Object, e As EventArgs)
+        ' Call the method to change the password of the selected user
+        ChangeUserPassword()
+    End Sub
+
+    Private Function GetUserData() As DataTable
+        Dim userData As New DataTable()
+
+        Try
+            ' Connect to the database
+            Using connection As New MySqlConnection(Form1.connectionString)
+                connection.Open()
+
+                ' Query to select user data
+                Dim query As String = "SELECT Username, Role FROM login"
+
+                ' Execute the query
+                Using cmd As New MySqlCommand(query, connection)
+                    Using adapter As New MySqlDataAdapter(cmd)
+                        ' Fill the DataTable with the results
+                        adapter.Fill(userData)
+                    End Using
+                End Using
+            End Using
+        Catch ex As Exception
+            Throw ex
+        End Try
+
+        Return userData
+    End Function
+
 End Class
